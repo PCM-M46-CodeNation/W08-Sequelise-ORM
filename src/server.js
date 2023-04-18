5a0fb1018d8b5d4ff0932c16932ab8668cae2993
ApacheTech Solutions <p_matthews@live.co.uk>
2023-04-18 15:30:20 +0100

    feat: add table synchronisation on launch

--- src/server.js
+++ src/server.js
@@ -3,6 +3,12 @@ const express = require("express");
 const app = express().use(express.json());
 const expressServerPort = process.env.EXPRESS_SERVER_PORT || 5001;
 
+const Book = require("./api/books/model");
+
+const syncTables = () => {
+	Book.sync();
+};
+
 app.use("/books", require("./api/books/routes"));
 
 app.get("/health", async (_, res) => {
@@ -10,5 +16,6 @@ app.get("/health", async (_, res) => {
 });
 
 app.listen(expressServerPort, () => {
+	syncTables();
 	console.log(`Express is listening on port ${expressServerPort}`);
 });
