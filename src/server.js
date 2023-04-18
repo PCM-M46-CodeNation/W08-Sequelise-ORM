6aa5a60b7d5e0db86c3c639d364ec08e56fa0590
ApacheTech Solutions <p_matthews@live.co.uk>
2023-04-18 14:35:01 +0100

    fix: set fallback server port

--- src/server.js
+++ src/server.js
@@ -1,13 +1,12 @@
 require("dotenv").config();
 const express = require("express");
 const app = express().use(express.json());
+const expressServerPort = process.env.EXPRESS_SERVER_PORT || 5001;
 
 app.get("/health", async (_, res) => {
 	res.status(200).json({ message: "Server is running." });
 });
 
-app.listen(process.env.EXPRESS_SERVER_PORT, () => {
-	console.log(
-		`Express is listening on port ${process.env.EXPRESS_SERVER_PORT}`,
-	);
+app.listen(expressServerPort, () => {
+	console.log(`Express is listening on port ${expressServerPort}`);
 });
