85ad2c10f6dbf7193c16356db8322d00b29ffba5
ApacheTech Solutions <p_matthews@live.co.uk>
2023-04-18 15:13:00 +0100

    feat: add empty book router

---
+++ src/api/books/routes.js
@@ -0,0 +1,4 @@
+const { Router } = require("express");
+const bookRouter = Router();
+
+module.exports = bookRouter;
--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ const express = require("express");
 const app = express().use(express.json());
 const expressServerPort = process.env.EXPRESS_SERVER_PORT || 5001;
 
+app.use("/books", require("./api/books/routes"));
+
 app.get("/health", async (_, res) => {
 	res.status(200).json({ message: "Server is running." });
 });
