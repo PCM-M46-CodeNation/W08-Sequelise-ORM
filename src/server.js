7ab0a8c336da49f948bb911f91780a151475620a
ApacheTech Solutions <p_matthews@live.co.uk>
2023-04-18 15:33:41 +0100

    refactor: clean server.js

--- src/server.js
+++ src/server.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
 const express = require("express");
-const app = express().use(express.json());
 const expressServerPort = process.env.EXPRESS_SERVER_PORT || 5001;
 
 const Book = require("./api/books/model");
@@ -9,13 +8,10 @@ const syncTables = () => {
 	Book.sync();
 };
 
-app.use("/books", require("./api/books/routes"));
-
-app.get("/health", async (_, res) => {
-	res.status(200).json({ message: "Server is running." });
-});
-
-app.listen(expressServerPort, () => {
-	syncTables();
-	console.log(`Express is listening on port ${expressServerPort}`);
-});
+express()
+	.use(express.json())
+	.use("/books", require("./api/books/routes"))
+	.listen(expressServerPort, () => {
+		syncTables();
+		console.log(`Express is listening on port ${expressServerPort}`);
+	});
