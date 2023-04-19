6902279118967383b0a99cc60824142a874f37af
ApacheTech Solutions <p_matthews@live.co.uk>
2023-04-19 12:32:29 +0100

    feat(books): add GET (ALL) endpoint

--- src/api/books/controllers/controller.queries.js
+++ src/api/books/controllers/controller.queries.js
@@ -1,3 +1,31 @@
 const Book = require("../model");
 
-module.exports = {};
+module.exports = {
+	/**
+	 * Get a list of all books, and sends a JSON response with a
+	 * success message and the list of books, or an error message and error object.
+	 *
+	 * @function
+	 * @name getAllBooks
+	 * @param {Object} req - The request object, un-used in this query.
+	 * @param {Object} res - The response object to send the JSON response.
+	 * @returns {Object} 200 - An array of all the books in the database.
+	 * @throws {Error} 501 - The details of the exception that was thrown.
+	 */
+	getAllBooks: async (req, res) => {
+		try {
+			const books = await Book.findAll({
+				include: { all: true, nested: true },
+			});
+			res.status(200).json({
+				message: "success",
+				books: books,
+			});
+		} catch (error) {
+			res.status(501).json({
+				message: error.message,
+				error: error,
+			});
+		}
+	},
+};
--- src/api/books/routes.js
+++ src/api/books/routes.js
@@ -4,5 +4,6 @@ const router = Router();
 const { commands, queries } = require("./controllers");
 
 router.post("/addbook", commands.addBook);
+router.get("/getallbooks", queries.getAllBooks);
 
 module.exports = router;
