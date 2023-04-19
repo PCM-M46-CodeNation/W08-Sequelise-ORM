0b0cd46a2a4f2167d8297a91d680461833c2a44d
ApacheTech Solutions <p_matthews@live.co.uk>
2023-04-19 12:52:18 +0100

    feat(books): add DELETE (SINGLE) endpoint

--- src/api/books/controllers/controller.commands.js
+++ src/api/books/controllers/controller.commands.js
@@ -27,4 +27,39 @@ module.exports = {
 				}),
 			);
 	},
+
+	/**
+	 * Deletes existing books, using the request body data and sends a JSON response with a
+	 * success message and the number of records removed, or an error message and error object.
+	 *
+	 * @function
+	 * @name deleteBook
+	 * @param {Object} req - The request object containing the book data in the request body.
+	 * @param {Object} res - The response object to send the JSON response.
+	 * @returns {Object} 201 - The number of records removed.
+	 * @throws {Error} 501 - The details of the exception that was thrown.
+	 */
+	deleteBook: (req, res) => {
+		Book.destroy({
+			where: {
+				title: req.body.title,
+			},
+		})
+			.then(n => {
+				if (n === 0)
+					throw new Error(
+						`No books found with title: ${req.body.title}`,
+					);
+				res.status(201).json({
+					message: "successfully deleted",
+					result: n,
+				});
+			})
+			.catch(e =>
+				res.status(501).json({
+					message: e.message,
+					error: e,
+				}),
+			);
+	},
 };
--- src/api/books/routes.js
+++ src/api/books/routes.js
@@ -7,4 +7,6 @@ router.post("/addbook", commands.addBook);
 router.get("/getallbooks", queries.getAllBooks);
 router.get("/getbook/:title", queries.getSingleBookByTitle);
 
+router.delete("/deletebook", commands.deleteBook);
+
 module.exports = router;
