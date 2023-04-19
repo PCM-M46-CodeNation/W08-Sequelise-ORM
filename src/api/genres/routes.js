c8390493ede82437a0a28a9540a926fc2b25ec91
ApacheTech Solutions <p_matthews@live.co.uk>
2023-04-19 11:37:54 +0100

    fix: ensure GET endpoint matches design docs

--- src/api/genres/routes.js
+++ src/api/genres/routes.js
@@ -4,6 +4,6 @@ const router = Router();
 const { commands, queries } = require("./controllers");
 
 router.post("/addgenre", commands.addGenre);
-router.get("/getbooksbygenre/:genre_id", queries.getAllBooks);
+router.get("/getbooksbygenre/:genre", queries.getAllBooks);
 
 module.exports = router;
