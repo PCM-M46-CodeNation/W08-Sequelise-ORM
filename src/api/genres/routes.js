268534353ce180bf405b80aa3a7290bc082602ce
ApacheTech Solutions <p_matthews@live.co.uk>
2023-04-19 10:44:50 +0100

    feat(genres): add GET enppoint

--- src/api/genres/routes.js
+++ src/api/genres/routes.js
@@ -4,5 +4,6 @@ const router = Router();
 const { commands, queries } = require("./controllers");
 
 router.post("/addgenre", commands.addGenre);
+router.get("/getbooksbygenre/:genre_id", queries.getAllBooks);
 
 module.exports = router;
