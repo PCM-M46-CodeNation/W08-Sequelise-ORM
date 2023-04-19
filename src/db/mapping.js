8d7c35e3cac58b44300fec6ebcd4de763c6cdb64
ApacheTech Solutions <p_matthews@live.co.uk>
2023-04-19 11:54:56 +0100

    fix: declare foriegn keys as not null

--- src/db/mapping.js
+++ src/db/mapping.js
@@ -6,20 +6,47 @@ const Genre = require("../api/genres/model");
  * Create table structure between DB and ORM.
  */
 const synchroniseTables = function () {
-	Author.sync();
-	Genre.sync();
-	Book.sync();
+	// NOTE: { alter: true } shouldn't be used within a production environment.
+	// It is a hack, to avoid needing to fully migrate the database, when making
+	// changes to the database structure. This will directly mutate database schema,
+	// potentially causing irrecoverable breaking changes, and data corruption.
+	// USE WITH CAUTION!
+	Author.sync({ alter: true });
+	Genre.sync({ alter: true });
+	Book.sync({ alter: true });
 };
 
 /**
  * Create relational mappings between tables.
  */
 const mapRelationships = function () {
-	Author.hasMany(Book, { foreignKey: "author_id" });
-	Book.belongsTo(Author, { foreignKey: "author_id" });
+	Author.hasMany(Book, {
+		foreignKey: {
+			name: "author_id",
+			allowNull: false,
+		},
+	});
 
-	Genre.hasMany(Book, { foreignKey: "genre_id" });
-	Book.belongsTo(Genre, { foreignKey: "genre_id" });
+	Genre.hasMany(Book, {
+		foreignKey: {
+			name: "genre_id",
+			allowNull: false,
+		},
+	});
+
+	Book.belongsTo(Author, {
+		foreignKey: {
+			name: "author_id",
+			allowNull: false,
+		},
+	});
+
+	Book.belongsTo(Genre, {
+		foreignKey: {
+			name: "genre_id",
+			allowNull: false,
+		},
+	});
 };
 
 module.exports = {
